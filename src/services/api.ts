3fe777ca0aace4b45337e7adaea58a15045b0d6d
bachvd <vuduybachvp@gmail.com>
2025-10-09 22:58:26 +0700

    bachvd

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://parking-zone-be.onrender.com/api';
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5002/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://parking-zone-be.onrender.com/api';
+// const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5002/api';
 
 
 const api = axios.create({
